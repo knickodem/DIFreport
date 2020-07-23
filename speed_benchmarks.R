fc5b43fcd023b4c5ad3dad35ed600e9487c96d14
Kyle Nickodem <kyle.nickodem@gmail.com>
2020-07-23 06:06:25 -0400

    Organization shift
    Broke up the primary executive function (DIF_analysis) into a middle manager wrapper for each analysis method (DIF_Methods_Wrappers)

--- speed_benchmarks.R
+++ speed_benchmarks.R
@@ -21,3 +21,40 @@ mb <- microbenchmark(
 # lapply 743.2580 769.0357 1355.836 1205.6590 1946.882 2210.239   100
 # map    742.1154 769.4960 1363.320  986.6364 1953.376 2286.834   100
 # loop   744.1545 769.8498 1412.430 1914.1626 1951.862 2197.985   100
+
+
+
+
+
+###########################################
+# Alternative approach using difR package #
+###########################################
+
+library(difR)
+
+#### MH ####
+
+MHdif <- difMH(Data = NumberRecog$MeasureData, group = NumberRecog$GroupVector,
+               focal.name = "Female", match = "score", MHstat = "logOR",
+               purify = TRUE, nrIter = 2, p.adjust.method = "BH")
+
+MHdif
+
+## Would need to write a function to extract the relevant info into the dataframe output we desire
+## This could also require running difMH twice, once with MHstat = "MHChisq", and once with "logOR"
+## Also, we should specify the vector for match rather than use the difMH default
+
+
+#### Logistic ###
+
+Logdif <- difLogistic(Data = NumberRecog$MeasureData, group = NumberRecog$GroupVector,
+                      focal.name = "Female", match = "score", type = "both", criterion = "Wald",
+                      p.adjust.method = "BH")
+
+Logdif
+
+## Again, would need to write a function to extract what we need since the difR output is disjointed
+## Need to review the workhorse functions to determine what procedures are exactly being used, and how many tests are conducted
+
+
+# Overall, using the difR package doesn't seem like it will save a whole lot of time.
